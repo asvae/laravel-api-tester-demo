62406cb5243faed011e2bc9b16a62356a77b85b2
Asvae <ontrew@gmail.com>
2016-08-22 16:01:43 +0300

    Add test routes.

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -2,7 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Debug\Dumper;
+
 class DemoController extends Controller
 {
-    public function
+    public function json()
+    {
+        return response()->json(['this', 'is' => 'json']);
+    }
+
+    public function varDump()
+    {
+        var_dump(['this', 'is' => 'var_dump']);
+    }
+
+    public function dd()
+    {
+        dd([
+            'several',
+            'values' => ['to', 'dump', 'and' => 'die'],
+        ]);
+    }
+
+    public function request(Request $request)
+    {
+        return $request->all();
+    }
+
+    public function redirect($times = 1)
+    {
+        if (! $times) {
+            return 'Redirections done';
+        }
+
+        return redirect()->route('redirect-test', ['times' => $times - 1]);
+    }
+
+    public function string()
+    {
+        return 'some static';
+    }
+
+    public function abort()
+    {
+        abort(418);
+    }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,6 +4,12 @@ Route::get('/', function () {
     return redirect('/api-tester');
 });
 
-Route::get('/', function () {
-    return redirect('/api-tester');
-});
+Route::get('redirect/{times}',
+    ['as' => 'redirect-test', 'uses' => 'DemoController@redirect']);
+Route::get('json','DemoController@json');
+Route::get('var_dump','DemoController@varDump');
+Route::get('json','DemoController@json');
+Route::get('string','DemoController@string');
+Route::get('abort','DemoController@abort');
+Route::get('i-dont-have-controller','MissingController@action');
+Route::get('i-dont-have-action','DemoController@action');
